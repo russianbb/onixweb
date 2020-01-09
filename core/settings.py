9ae3dfb1f0d462189fe0301ec41930e81e531207
RussianBB <bruno.russian.i@gmail.com>
2020-01-09 20:56:20 -0300

    Changing Settings

--- core/settings.py
+++ core/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'onixweb',
+            'USER': 'admin',
+            'PASSWORD': 'sol&OX16',
+            'HOST': 'localhost',
+            'PORT': '' 
+        }
+    }
 
 
 # Password validation
