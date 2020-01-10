c2ad1ccfe633923473f2aaa7ea7f23a9a51305c6
RussianBB <bruno.russian.i@gmail.com>
2020-01-09 21:01:03 -0300

    Rigth DB name

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_9&o9o+p4yim8a@*v#w$e(gzv$qh=&sq2rh%2sttcj8(d(w_&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '192.34.63.130', 'onixse.com', 'web.onixse.com']
 
@@ -84,7 +84,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'onixweb',
+            'NAME': 'onixwebdb',
             'USER': 'admin',
             'PASSWORD': 'sol&OX16',
             'HOST': 'localhost',
