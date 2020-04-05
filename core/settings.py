2b206e7a5daac1c96aa18e11f89715cc9688dbb2
Bruno Borges Russian <bruno.russian.i@gmail.com>
2020-04-04 22:07:25 -0300

    Pequenos ajustes de exibicao no front-end

--- core/settings.py
+++ core/settings.py
@@ -129,8 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static/')
 
 #My Settings
 LOGIN_URL = 'users:login'
